de0272977a69650ab586ebc9e2bcc4d28a0848c1
jenni.schubert@gmail.com <jennischubert@MacBook-Pro.local>
2020-08-07 16:57:54 -0500

    GET

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,7 +6,36 @@ function onReady () {
 console.log('jquery running');
 
 //add click listeners
-
+$('#submit').on('click', submitItem)
 //load data from server
+displayItems();
+}
+
+//get books from DB and append to table
+function displayItems() {
+    $('#displayList').empty();
+    $.ajax({
+        type: 'GET',
+        url: '/todo'
+    }).then(function (response) {
+        const taskList = response;
+        for (let i=0; i < taskList.length; i++) {
+            console.log(taskList);
+            $('#displayList').append(
+                `<tr data-id-${taskList[i].id}>
+                <td>${taskList[i].task}</td>
+                <td>${taskList[i].goal}</td>
+                <td>${taskList[i].status}</td>
+                <td></td>
+                <tr>
+                `
+            )
+        }
+    }).catch(function (error) {
+        console.log('error in GET', error)
+    });
+}
+
+function submitItem() {
 
 }
--- server/routes/to-do.router.js
+++ server/routes/to-do.router.js
@@ -3,8 +3,16 @@ const router = express.Router();
 
 const pool = require('../modules/pool');
 
-
-
+//get all tasks
+router.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM "items" ORDER BY "status" DESC;';
+    pool.query(queryText).then(result => {
+        res.send(result.rows);
+    }).catch(error => {
+        console.log('error getting tasks', error);
+        res.sendStatus(500);
+    });
+});
 
 
 
