7106da6a99b72a24ae2a2a0bac90b1c6ac1a4b2c
Jenni Schubert <jenni.schubert@gmail.com>
2020-08-10 19:11:55 -0500

    move pool to router test

--- server/routes/to-do.router.js
+++ server/routes/to-do.router.js
@@ -1,7 +1,45 @@
 const express = require('express');
 const router = express.Router();
 
-const pool = require('../modules/pool');
+// const pool = require('../modules/pool');
+
+
+const url = require('url');
+
+const Pool = pg.Pool;
+
+let config = {};
+
+if (process.env.DATABASE_URL) {
+
+  const params = url.parse(process.env.DATABASE_URL);
+  const auth = params.auth.split(':');
+
+  config = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true,
+    max: 10,
+    idleTimeoutMillis: 30000,
+  };
+} else {
+  config = {
+    database: 'weekend-to-do-app',
+    host: 'localhost',
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis: 30000
+  };
+}
+
+const pool = new Pool(config);
+
+
+
+
 
 //delete items
 router.delete('/:id', (req, res) => {
